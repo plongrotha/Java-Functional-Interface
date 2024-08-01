efaec4be9d0b5ae44933b48ddf47e1338f883fa3
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-01 14:38:40 +0700

    added 2 file

--- src/functionalInterface/FunctionalInterfaceDemo.java
+++ src/functionalInterface/FunctionalInterfaceDemo.java
@@ -1,5 +1,14 @@
 package functionalInterface;
 
+import mpl.AddFive;
+import mpl.AddFiveImpl;
+
 public class FunctionalInterfaceDemo {
 
+  public static void main(String[] args) {
+
+    AddFive addFive = new AddFiveImpl();
+    int result = addFive.add(20);
+    System.out.println(result);
+  }
 }
---
+++ src/mpl/AddFiveImpl.java
@@ -0,0 +1,10 @@
+package mpl;
+
+public class AddFiveImpl implements AddFive {
+
+  @Override
+  public int add(int number) {
+    return number + 5;
+  }
+
+}
